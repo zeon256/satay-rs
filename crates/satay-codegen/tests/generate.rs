c6d126d8b91bf9438a8dcd5fb4a2a6d49d14773d
Budi Syahiddin <me@inve.rs>
2026-06-09 20:35:30 +0800

    feat(codegen): make sure datetimes dont get additional reference

--- crates/satay-codegen/tests/generate.rs
+++ crates/satay-codegen/tests/generate.rs
@@ -1123,7 +1123,7 @@ paths:
     assert!(
         parts_rs
             .contents
-            .contains("satay_runtime::format_date(&value)")
+            .contains("satay_runtime::format_date(value)")
     );
 
     let temp = tempfile::tempdir().expect("create temp crate");
@@ -1193,7 +1193,7 @@ paths:
     assert!(
         parts_rs
             .contents
-            .contains("satay_runtime::format_naive_datetime(&value)")
+            .contains("satay_runtime::format_naive_datetime(value)")
     );
 
     let temp = tempfile::tempdir().expect("create temp crate");
