4bbdac23de6327165c8f9b89651f350efb4739f1
Budi Syahiddin <me@inve.rs>
2026-09-19 23:42:04 +0800

    refactor(runtime): move inline test modules to sibling files

---
+++ crates/satay-runtime/src/tests/optional_f64_none_if.rs
@@ -0,0 +1,16 @@
+use crate::serde_string::as_f64::{self, option};
+
+#[allow(clippy::ref_option)]
+pub fn serialize<S>(value: &Option<f64>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    as_f64::serialize_none_if(value, "NA", serializer)
+}
+
+pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<f64>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    option::deserialize_none_if(deserializer, &["NA", "-"])
+}
